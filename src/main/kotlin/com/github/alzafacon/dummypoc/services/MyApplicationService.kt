d227fb503bbe4166bc6cd97a4adc62817be1d383
GitHub Action <action@github.com>
2023-02-06 19:42:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/alzafacon/dummypoc/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alzafacon/dummypoc/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alzafacon.dummypoc.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alzafacon.dummypoc.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/alzafacon/dummypoc/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alzafacon/dummypoc/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alzafacon.dummypoc.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alzafacon.dummypoc.MyBundle
 
 class MyProjectService(project: Project) {
 
